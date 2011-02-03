87312e06027ddd7b1cd600fda784efdbf4c36c84
tgummerer <t.gummerer@gmail.com>
2011-02-03 11:42:06 +0100

    Configuration file can be written

---
+++ client/helpers/makeconfig.py
@@ -0,0 +1,31 @@
+# Create a configuration file
+#   Copyright (C) 2011 Thomas Gummerer
+#
+# This file is part of Filesync.
+# 
+# Filesync is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# Filesync is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with Filesync.  If not, see <http://www.gnu.org/licenses/>. 
+
+# configparser used to write the config file
+import configparser;
+config = configparser.ConfigParser()
+
+# Get the input
+syncdir = input("Please enter the path to the directory that should be syncronized: ")
+
+config['config'] = {}
+config['config']['syncpath'] = syncdir
+with open('../config.ini', 'w') as configfile:
+	config.write(configfile)
+
+

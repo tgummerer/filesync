dd4c3c26526d6f83da6b3e4213a4b727a31cb2b4
tgummerer <t.gummerer@gmail.com>
2011-02-05 15:26:50 +0100

    Create script for the server database

---
+++ server/sql/create.sql
@@ -0,0 +1,28 @@
+CREATE TABLE "user"
+(
+  userid integer NOT NULL DEFAULT nextval('user_id_seq'::regclass),
+  email text NOT NULL,
+  "password" text NOT NULL,
+  CONSTRAINT id PRIMARY KEY (userid),
+  CONSTRAINT email UNIQUE (email)
+)
+WITH (
+  OIDS=FALSE
+);
+
+CREATE TABLE file
+(
+  fileid serial NOT NULL,
+  userid integer NOT NULL,
+  path text NOT NULL,
+  lastchange timestamp without time zone,
+  CONSTRAINT fileid PRIMARY KEY (fileid),
+  CONSTRAINT userid FOREIGN KEY (userid)
+      REFERENCES "user" (userid) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT path UNIQUE (path)
+)
+WITH (
+  OIDS=FALSE
+);
+

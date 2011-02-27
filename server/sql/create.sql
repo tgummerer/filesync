5ba6eeb1ad8e2e650134519d3daa06f39c8733f3
tgummerer <t.gummerer@gmail.com>
2011-02-27 12:28:15 +0100

    Updated database creation script. Ready for distributing the first alpha

--- server/sql/create.sql
+++ server/sql/create.sql
@@ -1,28 +1,57 @@
 CREATE TABLE usertable
 (
-  userid integer NOT NULL DEFAULT nextval('user_id_seq'::regclass),
-  email text NOT NULL,
-  "password" text NOT NULL,
-  CONSTRAINT id PRIMARY KEY (userid),
-  CONSTRAINT email UNIQUE (email)
+	userid serial NOT NULL,
+	email text NOT NULL,
+	"password" text NOT NULL,
+	CONSTRAINT id PRIMARY KEY (userid),
+	CONSTRAINT email UNIQUE (email)
 )
 WITH (
-  OIDS=FALSE
+	OIDS=FALSE
 );
 
-CREATE TABLE file
+CREATE TABLE client
 (
-  fileid serial NOT NULL,
-  userid integer NOT NULL,
-  path text NOT NULL,
-  lastchange timestamp without time zone,
-  CONSTRAINT fileid PRIMARY KEY (fileid),
-  CONSTRAINT userid FOREIGN KEY (userid)
-      REFERENCES "user" (userid) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION,
-  CONSTRAINT path UNIQUE (path)
+	clientid serial NOT NULL,
+	userid integer,
+	CONSTRAINT primarykey PRIMARY KEY (clientid),
+	CONSTRAINT userid FOREIGN KEY (userid)
+	REFERENCES usertable (userid) MATCH SIMPLE
+	ON UPDATE NO ACTION ON DELETE NO ACTION
 )
 WITH (
-  OIDS=FALSE
+	OIDS=FALSE
+);
+
+CREATE TABLE filetable
+(
+	fileid serial NOT NULL,
+	userid integer NOT NULL,
+	path text NOT NULL,
+	lastchange timestamp without time zone DEFAULT now(),
+	CONSTRAINT fileid PRIMARY KEY (fileid),
+	CONSTRAINT userid FOREIGN KEY (userid)
+	REFERENCES usertable (userid) MATCH SIMPLE
+	ON UPDATE NO ACTION ON DELETE NO ACTION,
+	CONSTRAINT path UNIQUE (path)
+)
+WITH (
+	OIDS=FALSE
+);
+
+CREATE TABLE hasnewest
+(
+	clientid integer NOT NULL,
+	fileid integer NOT NULL,
+	CONSTRAINT hasnewest_pkey PRIMARY KEY (clientid, fileid),
+	CONSTRAINT client FOREIGN KEY (clientid)
+	REFERENCES client (clientid) MATCH SIMPLE
+	ON UPDATE NO ACTION ON DELETE NO ACTION,
+	CONSTRAINT file FOREIGN KEY (fileid)
+	REFERENCES filetable (fileid) MATCH SIMPLE
+	ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+	OIDS=FALSE
 );
 
